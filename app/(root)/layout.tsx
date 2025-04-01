57eb93482ca1ec63ae10bd343715b01b0e04debd
Kitui Kelly <kituikelly@gmail.com>
2025-04-01 09:00:08 +0300

    adds the home button

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import {ReactNode} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import LogoutButton from '@/components/ui/signout'
+import { Button } from "@/components/ui/button";
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
@@ -12,6 +13,15 @@ const RootLayout = ({children}: {children: ReactNode}) => {
                 <h2 className='text-primary-100'>PrepWise</h2>
             </Link>
             <LogoutButton />
+
+
+            <Button className="btn-secondary flex-1">
+              <Link href="/" className="flex w-full justify-center">
+                <p className="text-sm font-semibold text-primary-200 text-center">
+                  Home
+                </p>
+              </Link>
+            </Button>
         </nav>
         {children}
     </div>
